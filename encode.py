93e2275963656cfa765b330dc9be915efde31c53
rotem-vaknin <rot.vaknin@gmail.com>
2020-11-29 12:40:19 +0200

    Update encode.py

--- encode.py
+++ encode.py
@@ -6,8 +6,8 @@
 import argparse
 import numpy as np
 
-import encoder
-from load_dataset import load_dataset
+import src.encoder as encoder
+from src.load_dataset import load_dataset
 
 parser = argparse.ArgumentParser(
     description='Pre-encode text files into tokenized training set.',
